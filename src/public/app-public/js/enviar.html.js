264edb2210d0dc30bc362ae1abcf99b8d457b32e
edcan-dev <israel.da.queenie@gmail.com>
2023-03-03 01:06:35 -0600

    final

--- src/public/app-public/js/enviar.html.js
+++ src/public/app-public/js/enviar.html.js
@@ -26,9 +26,9 @@ let sendVacantesArray = [];
 
         vacantesTable = newTable;
     }
-    const unidadesEco = await getUnidadesEco("/app/vacantes");
+    const vacantes = await fetchData("/api/vacantes");
     //console.log(candidatos);
-    printData(unidadesEco);
+    printData(vacantes);
 
     vacantesContainer.firstElementChild.innerHTML = 'Listado de Vacantes';
 
@@ -37,7 +37,6 @@ let sendVacantesArray = [];
     // Habilitando contenedor de descarga de informe
     // descargaContainer.classList.remove("display-none");
 
-    const enlaceVacantes = "https://docs.google.com/spreadsheets/d/1jWtWT8FqSZCOkhYJckZB8Rimx0P0j9FqbVOdazhY6CQ/edit?usp=sharing";
     
 
     //descargaContainer.firstElementChild.innerHTML = "Acceso al informe completo";
@@ -45,11 +44,10 @@ let sendVacantesArray = [];
     // document.querySelector(".descarga_container > a").href = enlaceVacantes;
 })();
 
-async function getUnidadesEco(anchorHREF) {
+async function fetchData(apiURL) {
 
     let vacantes = {};
 
-    const apiURL = '/api/vacantes';
     //console.log(apiURL);    
 
     await fetch(apiURL) // Pide datos de la API
@@ -62,9 +60,12 @@ async function getUnidadesEco(anchorHREF) {
     return vacantes;
 }
 
-function printData(data){
+async function printData(data) {
 
-    console.log(data);
+    // console.log(data);
+
+    const internosData = await fetchData("/api/can-internos");
+    const externosData = await fetchData("/api/can-externos");
 
     const headerRow = data[0];
     
@@ -77,7 +78,7 @@ function printData(data){
     headerRow.forEach((element, index)=> {
 
 
-        console.log(element);
+        //console.log(element);
 
         if(headerMainData.includes(element)) { 
             headerMainDataIndex.push(index);
@@ -88,12 +89,8 @@ function printData(data){
         }
             
     });
-
-    console.log(headerMainDataIndex);
-
     
-
-      for (const element in data) {
+    for (const element in data) {
         if(element != 0) {
             //console.log(data[element]);
             const contentRow = document.createElement('tr');
@@ -114,31 +111,213 @@ function printData(data){
             contentRow.appendChild(rowCell).appendChild(sendVacantesLink);
             sendVacantesLink.innerHTML ="Consultar candidatos";
             sendVacantesLink.href = "/app/enviar";
-            sendVacantesButton = sendVacantesLink;
+            sendVacantesLink.classList.add("consultar_button");
 
-            const idVacante = data.indexOf(element);
+            fetchedData = data;
 
-            const sendVacantesObject = {
-                 idVacante: idVacante,
-                sendVacantes: sendVacantesButton
-            };
-
-            // Conseguir indice del elemento en el data
-
-            sendVacantesArray.push(sendVacantesObject);
+            sendVacantesArray.push({
+                "vacante":element,
+                "element":sendVacantesLink
+            });
         }
 
-        
     }
+    var consultarButtons = sendVacantesArray.map(e=> e.element);
+    // console.log(consultarButtons);
+    
+    consultarButtons.forEach((button,index)=> {
 
-    goToVacantesDetails(sendVacantesArray)
+        button.addEventListener("click",(e)=> {
+            e.preventDefault();
+
+            let numeroDeVacante = consultarButtons.indexOf(button) + 1;
+
+            const vacanteLowed = arrayToLowerCase(data[numeroDeVacante]);
+            const internosLowed = objectToLowerCase(internosData);
+            const externosLowed = objectToLowerCase(externosData);
 
 
+            //console.log(vacanteLowed);
+            //console.log(internosLowed);
+            //console.log(externosLowed);
 
+            const vacantePalabras = splitVacante(vacanteLowed);
+
+            const internosPalabras = internosLowed.map(e => splitVacante(e));
+            const externosPalabras = externosLowed.map(e => splitVacante(e));
+
+            //internosLowed.forEach(e => console.log(e));
+
+            //internosLowed.forEach(e => console.log(e[1]));
+            //console.log(vacantePalabras);
+            //console.log(internosPalabras);
+            //console.log(externosPalabras);
+            //console.log(arrayToLowerCase(externosData));
+
+            const  ocurrencesInternos = findOcurrences(vacantePalabras, internosPalabras);
+            const  ocurrencesExternos = findOcurrences(vacantePalabras, externosPalabras);
+
+            const internosDataArray = Object.entries(internosData);
+            const externosDataArray = Object.entries(externosData);
+
+
+            // AGREGA EL FACTOR DE COINCIDENCIAS
+            internosDataArray.forEach( (interno, index) => interno.push(ocurrencesInternos[index].ocurrences));
+            externosDataArray.forEach( (externo, index) => externo.push(ocurrencesExternos[index].ocurrences));
+
+            // ORDENA POR EL FACTOR DE COINCIDENCIAS
+
+            //console.log(internosDataArray);
+
+            //internosDataArray.forEach(e => console.log(e[2]))
+
+            const sortedInternosResult = internosDataArray.sort(sortOcurrences);
+            const sortedExternosResult = externosDataArray.sort(sortOcurrences);
+
+            //console.log(sortedExternosResult);
+
+            
+
+            let resultTable = document.querySelector(".result_table");
+                if(resultTable.childElementCount != 0) {
+                    resultTable.remove();
+                    resultTable = document.createElement("table");
+                    resultTable.classList.add("result_table");
+                    document.querySelector(".result_container").appendChild(resultTable);
+                } 
+                    
+                    sortedInternosResult.forEach( e => {
+                        
+                        const resultRow = document.createElement("tr");
+
+                        
+                        const resultCell1 = document.createElement("td");
+                        resultCell1.innerHTML = e[1][1];
+                        const resultCell2 = document.createElement("td");
+                        resultCell2.innerHTML = e[1][2];
+                        const resultCell3 = document.createElement("td");
+                        resultCell3.innerHTML = e[1][3];
+                        const resultCell4 = document.createElement("td");
+                        resultCell4.innerHTML = e[1][4];
+                        const resultCell5 = document.createElement("td");
+                        resultCell5.innerHTML = e[1][11];
+                        
+                        resultRow.appendChild(resultCell1);
+                        resultRow.appendChild(resultCell2);
+                        resultRow.appendChild(resultCell3);
+                        resultRow.appendChild(resultCell4);
+                        resultRow.appendChild(resultCell5);
+                        
+                        if(e[0] != "0"){
+                            document.querySelector(".result_table").appendChild(resultRow);
+                        }
+                    });
+                    sortedExternosResult.forEach( e => {
+                        
+                        const resultRow = document.createElement("tr");                        
+                        
+                        const resultCell1 = document.createElement("td");
+                        resultCell1.innerHTML = e[1][1];
+                        const resultCell2 = document.createElement("td");
+                        resultCell2.innerHTML = e[1][2];
+                        const resultCell3 = document.createElement("td");
+                        resultCell3.innerHTML = e[1][3];
+                        const resultCell4 = document.createElement("td");
+                        resultCell4.innerHTML = e[1][4];
+                        const resultCell5 = document.createElement("td");
+                        resultCell5.innerHTML = e[1][11];
+                        
+                        resultRow.appendChild(resultCell1);
+                        resultRow.appendChild(resultCell2);
+                        resultRow.appendChild(resultCell3);
+                        resultRow.appendChild(resultCell4);
+                        resultRow.appendChild(resultCell5);
+                        
+                        if(e[0] != "0"){
+                            document.querySelector(".result_table").appendChild(resultRow);
+                        }
+                    });
+                    
+                
+
+
+        
+            })
+    });
+}
+
+function sortOcurrences(a,b) {
+    if(a[2]> b[2]) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+function findOcurrences(vacanteArray, candidatosArraysArray) {
+
+    const result = [];
+
+
+    candidatosArraysArray.forEach( (candidatoArray, index) =>{
+
+        let ocurrences = 0;
+        
+        vacanteArray.forEach((vacanteWord, index)=> {
+            
+            candidatoArray.forEach((candidatoWord) => {
+
+                if(candidatoWord == vacanteWord) ocurrences ++;
+            });
+
+        });
+
+        result.push({index,ocurrences});
+    })
+    return result;
+}
+
+function splitVacante(array) {
+    const vacantePalabras = [];
+    array.forEach(e => {
+        const splitedPalabra = e.split(" ");
+        if(e != splitedPalabra) {
+            //console.log(splitedPalabra);
+            splitedPalabra.forEach(e => {
+                if(e.includes('.')) {
+                    var eCleared = e.replace('.','');
+                } else {
+                    eCleared = e;
+                }
+                if(eCleared != '') {
+                    vacantePalabras.push(eCleared)
+                }
+            });
+        } else {
+            if(e != '') {   
+                vacantePalabras.push(e);
+            }
+        }
+    });
+    return vacantePalabras;
+}
+
+function objectToLowerCase(object) {
+    const array = Object.entries(object);
+    
+    const arrayLowed = array.map(e => arrayToLowerCase(e[1]));
+    return arrayLowed
+}
+
+function arrayToLowerCase(array) {
+    const arrayLowerCased = array.map( e => e.toLowerCase());
+    return arrayLowerCased;
 }
 
 function goToVacantesDetails(array) {
     console.log(array);
 
 }
+
+
 
